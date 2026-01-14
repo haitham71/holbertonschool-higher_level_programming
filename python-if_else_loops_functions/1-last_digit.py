21c262d02469da368985b0a7f3c9520839db30cc
haitham71 <hri7isd@gmail.com>
2026-01-14 14:21:09 +0300

    Fix calculation of last digit in 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 zero = 0
-num1 = abs(number) % 10
+num1 = number % 10 if number >= 0 else -(abs(number) % 10)
 if num1 > 5:
 	print(f"Last digit of {number} is {num1} and is greater than 5")
 elif num1 == zero :
