2c59adbb7b1da8459565e1b721c0551ee990653a
haitham71 <hri7isd@gmail.com>
2026-01-14 14:22:38 +0300

    Fix formatting and indentation in last digit output messages in 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
 zero = 0
 num1 = number % 10 if number >= 0 else -(abs(number) % 10)
+
 if num1 > 5:
-	print(f"Last digit of {number} is {num1} and is greater than 5")
-elif num1 == zero :
+    print(f"Last digit of {number} is {num1} and is greater than 5")
+elif num1 == zero:
     print(f"Last digit of {number} is {num1} and is {zero}")
-elif num1 < 6:
-    print(f"Last digit of {number} is {num1} and is less than 6 and not {zero}")
+else:
+    print(
+        f"Last digit of {number} is {num1} "
+        f"and is less than 6 and not {zero}"
+    )
