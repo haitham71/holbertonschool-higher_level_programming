811cfd1d738b8a1092c1a3fb7b77d7a6f4333f4a
haitham71 <hri7isd@gmail.com>
2026-01-20 17:11:21 +0300

    a

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0 :
+    if len(sentence) == 0:
         return (0, None)
     length = len(sentence)
     first = sentence[0]
