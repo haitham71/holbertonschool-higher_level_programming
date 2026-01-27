3591b600897db0d47f63c9a1cedf9cd71c52514f
haitham71 <hri7isd@gmail.com>
2026-01-27 19:51:21 +0300

    aa

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Defines a Square class with size, area, and print functionality.
+"""
+
+
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size with validation"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print the square using #"""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
