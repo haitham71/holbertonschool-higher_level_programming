4a4cea1f21aeb0b95520b8335b07c62fd38b14a3
haitham71 <hri7isd@gmail.com>
2026-01-27 16:55:01 +0300

    hi

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
 """
-diffrent module same name
+new module in square make a function
 """
 
 
 class Square:
     """
-    parameter have init value 0, private attribute,
-    raise the error when its happen
+    compute the area of a square
     """
 
     def __init__(self, size=0):
         """
 
-        :param size: the size of the square
+        :param size: square size to be computed
+
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-
         else:
-            self._Square__size = size
+            self.__size = size
+
+    def area(self):
+        """
+        compute the area of a square
+        :return: result
+        """
+        return self.__size * self.__size
