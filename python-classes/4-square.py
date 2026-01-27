45c13f872c4dfc32a2321d54d8be250dab743421
haitham71 <hri7isd@gmail.com>
2026-01-27 19:44:24 +0300

    jak

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,28 +1,47 @@
 #!/usr/bin/python3
-"""
-Defines a Square class with private size attribute,
-getter, setter, and area calculation.
-"""
+"""Defines a Square class with size validation."""
 
 
 class Square:
+    """
+    first use of property
+    """
+
     def __init__(self, size=0):
-        self.size = size 
+        """
+        :param self: need in all methods
+        :param size: the value of it
+        :return: size amount
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        :return: area amount
+        """
+        return self.__size * self.__size
 
     @property
     def size(self):
-        """Retrieve the size"""
+        """
+
+        :return: size amount
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the size with validation"""
+        """
+        :param value: assigned to size
+        :return: __size
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    def area(self):
-        """Return the area of the square"""
-        return self.__size * self.__size
