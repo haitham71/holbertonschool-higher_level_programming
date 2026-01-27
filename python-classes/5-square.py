471b0d9d0d5895bdcd5b3ecfbd92cb5cda813f37
haitham71 <hri7isd@gmail.com>
2026-01-27 19:55:07 +0300

    test

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -6,7 +6,12 @@ Defines a Square class with size, area, and print functionality.
 
 class Square:
     def __init__(self, size=0):
-        self.size = size 
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     @property
     def size(self):
