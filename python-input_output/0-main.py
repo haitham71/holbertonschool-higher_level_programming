d6f51bf473d4f88d22456bd3b5d2d9ad9397ed88
haitham71 <hri7isd@gmail.com>
2026-02-02 13:42:53 +0300

    AA

--- python-input_output/0-main.py
+++ python-input_output/0-main.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""
-Reads a text file (UTF-8) and prints it to stdout.
-"""
 read_file = __import__('0-read_file').read_file
-"""
-Reads a text file and prints its content to stdout.
-"""
+
 read_file("my_file_0.txt")
--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""
+Reads a text file (UTF-8) and prints it to stdout.
+"""
+
 
 def read_file(filename=""):
-
     """
- Reads a text file (UTF-8) and prints it to stdout.
-"""
-with open(filename, "r", encoding="utf-8") as f:
-    print(f.read(), end="")
+    Reads a text file and prints its content to stdout.
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
