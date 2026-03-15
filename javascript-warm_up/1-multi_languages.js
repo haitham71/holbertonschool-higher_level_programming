4ee46f33fd52ea16177e604886cc157651dd56ae
haitham71 <hri7isd@gmail.com>
2026-03-15 07:22:07 +0300

    js on fire

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar = 'JavaScript is amazing';
+const myVar2 = 'C is fun';
+const myVar3 = 'Python is cool';
+console.log(myVar2);
+console.log(myVar3);
+console.log(myVar);
