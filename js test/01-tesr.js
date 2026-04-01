df424ffee5ec4ab9b1bd8fbdbdcd717130514b20
haitham71 <hri7isd@gmail.com>
2026-04-01 13:17:33 +0300

    aa

---
+++ js test/01-tesr.js
@@ -0,0 +1,25 @@
+//let name = ["me", "you", "him", 2];
+//console.log(name.length-1);
+
+// let colors = ['red', 'green', 'blue'];
+// let blueColor = colors.pop();
+// console.log(blueColor);
+// console.log(colors)
+
+// let colors = ['red', 'green', 'blue'];
+// const redColor = colors.shift();
+// const redColor1 = colors.shift();
+// const redColor2 = colors.shift();
+// console.log(redColor);
+// console.log(redColor1);
+// console.log(redColor2);
+
+
+let num = 0;
+if (num > 0){
+	console.log('+')
+}else if (num < 0){
+	console.log('-')
+}else{
+	console.log('=')
+}
